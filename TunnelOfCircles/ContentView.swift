c9486af6d2f24087ab9262dde28ec859825e2b9e
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2024-04-29 15:18:06 -0500

    Initial Commit

---
+++ TunnelOfCircles/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TunnelOfCircles
+//
+//  Created by Justin Hold on 4/29/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ TunnelOfCircles/TunnelOfCirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TunnelOfCirclesApp.swift
+//  TunnelOfCircles
+//
+//  Created by Justin Hold on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct TunnelOfCirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
